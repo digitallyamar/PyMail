4e0f9916ceb6dd5cd831ccc5257ca645e1fb763e
digitallyamar <digitallyamar@gmail.com>
2021-05-12 15:48:45 +0530

    Email with HTML support.

--- pymail.py
+++ pymail.py
@@ -1,18 +1,50 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 port = 465  # For SSL
+
+sender_email = "amar@gmail.com"
+receiver_email = "amarnath@gmail.com"
+
 password = input("Type your password and press enter: ")
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "Welcome To My Email!"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+
+# Create the plain-text and HTML version of your message
+text = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       Welcome to <a href="https://google.com">Google</a>!
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("amar@gmail.com", password)
-    # TODO: Send email here
-    sender_email = "amar@gmail.com"
-    receiver_email = "amarnath@gmail.com"
-    message = """\
-    Subject: Hi there
-
-    This message is sent as a test mail."""
-    server.sendmail(sender_email, receiver_email, message)
+    server.login("fitwithamar@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
